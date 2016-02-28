7fe424a0e9371f39544e9c136a2a82b6ca053e65
giriash <linzhe940604@gmail.com>
2016-02-28 11:05:21 -0500

    Revert "There is a problem in reading .csv file."
    This reverts commit 88941016d163011464b624837e2e38c8a05b27d4.

--- js/map.js
+++ js/map.js
@@ -8,71 +8,6 @@ var svg = d3.select("#map").append("svg")
     .attr("width", width)
     .attr("height", height);
 var g = svg.append("g");
-//var worldObjects;
-
-var digitScale = d3.scale.category20b();
-
-var allDataMap;
-//var pationRate;
-
-//read json file
-d3.json("json/world-50m.json", function(error, world) {
-  //worldObjects = world;
-  
-  if(error) {
-    console.log(error);
-  }
-
-  var countries = topojson.feature(world,world.objects.countries).features;
-
-  //countries.forEach(function(country){
-    g.selectAll("path").data(countries).enter()
-    .append("path").attr("d",path)
-    .style("fill", function (c){ return digitScale(Math.floor(c.id/5)); })//"#192128")
-    .style("stroke","#2A2F3B");
-  //}); 
-
-  g.selectAll("path").data(countries)
-  .on("mouseover", function (country){
-    console.log(country.id);
-  });
-
-  d3.csv("WomenInWorkforce.csv",function(error,data){ //WomenInWorkforce.csv
-    if (error) {console.log(error);}
-    //console.log(rows);
-
-    allDataMap = d3.map(data, function (county) { return Number(county.ISOID); });
-    console.log(allDataMap); //undefined
-    g.selectAll("path")
-    .style("fill",function(c){
-      var countryData = allDataMap.get(c.id);//only get one id
-      console.log(countryData);
-      console.log(c.id);
-      var pationRate = countryData.LaborForce;
-      if (!countryData) { return "#2A2F3B";}
-      else if (pationRate <= 30) { return "#000000";}
-      else if (pationRate > 30 && pationRate <=50) { return "#333333";}
-      else if (pationRate > 50 && pationRate <=70) { return "#777777";}
-      else if (pationRate > 70 && pationRate <=100) { return "#7bfbfbf";}
-
-      return "#192128";
-    })
-  });
-
-});
-
-
-
-/**var width = 1056,
-    height = 550;
-
-var projection = d3.geo.mercator();
-var path = d3.geo.path().projection(projection);
-  
-var svg = d3.select("#map").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-var g = svg.append("g");
 var worldObjects;
 
 //read json file
@@ -113,5 +48,5 @@ d3.json("json/world-50m.json", function(error, world) {
   .on("mouseover", function (country){
     console.log(country.id);
   });
-});**/
+});
   console.log("after json");
