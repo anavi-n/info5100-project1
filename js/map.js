8f7cd93a985e1c1526d84a20e1695fb2d689112e
giriash <linzhe940604@gmail.com>
2016-03-02 18:22:57 -0500

    final version 1

--- js/map.js
+++ js/map.js
@@ -4,7 +4,7 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
 
 var projection = d3.geo.mercator();
 var path = d3.geo.path().projection(projection);
-  
+
 var svg = d3.select("#map1").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -22,7 +22,7 @@ var isoidData;
 //read json file
 d3.json("json/world-50m.json", function(error, world) {
   //worldObjects = world;
-  
+
   if(error) {
     console.log(error);
   }
@@ -32,23 +32,84 @@ d3.json("json/world-50m.json", function(error, world) {
     //add shadow to stroke
   var filter = g.append("filter")
         .attr("id", "stroke-shadow")
-        .attr("height", "140%");  
+        .attr("height", "140%");
+      
+      filter.append("feGaussianBlur")
+          .attr("in", "SourceAlpha")
+          .attr("stdDeviation", 2)
+          .attr("result", "blur");
 
-    filter.append("feGaussianBlur")
-    .attr("in", "SourceAlpha")
-    .attr("stdDeviation", 2)
-    .attr("result", "blur");
-
-    filter.append("feOffset")
-    .attr("in", "blur")
-    .attr("dx", 2)
-    .attr("dy", 2)
-    .attr("result", "offsetBlur");
+      filter.append("feOffset")
+          .attr("in", "blur")
+          .attr("dx", 0.2)
+          .attr("dy", 0.5)
+          .attr("result", "offsetBlur");
 
     var feMerge = filter.append("feMerge");
 
     feMerge.append("feMergeNode").attr("in", "offsetBlur");
     feMerge.append("feMergeNode").attr("in", "SourceGraphic");
+
+      var filter2 = g.append("filter")
+        .attr("id", "stroke-shadow2")
+        .attr("height", "140%");
+      
+      filter2.append("feGaussianBlur")
+          .attr("in", "SourceAlpha")
+          .attr("stdDeviation", 2)
+          .attr("result", "blur");
+
+      filter2.append("feOffset")
+          .attr("in", "blur")
+          .attr("dx", 0.5)
+          .attr("dy", 1)
+          .attr("result", "offsetBlur");
+
+    var feMerge2 = filter2.append("feMerge");
+
+    feMerge2.append("feMergeNode").attr("in", "offsetBlur");
+    feMerge2.append("feMergeNode").attr("in", "SourceGraphic");
+
+    var filter3 = g.append("filter")
+        .attr("id", "stroke-shadow3")
+        .attr("height", "140%");
+      
+      filter3.append("feGaussianBlur")
+          .attr("in", "SourceAlpha")
+          .attr("stdDeviation", 2)
+          .attr("result", "blur");
+
+      filter3.append("feOffset")
+          .attr("in", "blur")
+          .attr("dx", 0.8)
+          .attr("dy", 1.5)
+          .attr("result", "offsetBlur");
+
+    var feMerge3 = filter3.append("feMerge");
+
+    feMerge3.append("feMergeNode").attr("in", "offsetBlur");
+    feMerge3.append("feMergeNode").attr("in", "SourceGraphic");
+
+    var filter4 = g.append("filter")
+        .attr("id", "stroke-shadow4")
+        .attr("height", "140%");
+      
+      filter4.append("feGaussianBlur")
+          .attr("in", "SourceAlpha")
+          .attr("stdDeviation", 2)
+          .attr("result", "blur");
+
+      filter4.append("feOffset")
+          .attr("in", "blur")
+          .attr("dx", 1.1)
+          .attr("dy", 2.5)
+          .attr("result", "offsetBlur");
+
+    var feMerge4 = filter4.append("feMerge");
+
+    feMerge4.append("feMergeNode").attr("in", "offsetBlur");
+    feMerge4.append("feMergeNode").attr("in", "SourceGraphic");
+
 
       //countries.forEach(function(country){
     g.selectAll("path").data(countries).enter()
@@ -64,134 +125,125 @@ d3.json("json/world-50m.json", function(error, world) {
     //console.log(rows);
     isoidData = d3.map(data, function (county) { return Number(county.ISOID); //console.log(county.ISOID)
     });
-    console.log(isoidData); 
+    console.log(isoidData);
     g.selectAll("path")
     //.style("opacity","0.8")
     .style("fill",function(d){
       var countryData = isoidData.get(d.id);//only get one id
+      console.log(countryData); //undefined
+      console.log(d.id);
       //var pationRate = countryData.LaborForce;
       if (!countryData) { return "#DDDDDD";}
-      else if (countryData.LaborForce <= 20) { return "#738AB0";}
-      else if (countryData.LaborForce > 20 && countryData.LaborForce <=40) { return "#C9CFDB";}
-      else if (countryData.LaborForce > 40 && countryData.LaborForce <=60) { return "#F0E3E8";}
-      else if (countryData.LaborForce > 60 && countryData.LaborForce <=80) { return "#E7CAD5";}
-      else if (countryData.LaborForce > 80 && countryData.LaborForce <=100) { return "#9C4769";}
+      else if (countryData.LaborForce <= 20) { return "#feebe2";}
+      else if (countryData.LaborForce > 20 && countryData.LaborForce <=40) { return "#fbb4b9";}
+      else if (countryData.LaborForce > 40 && countryData.LaborForce <=60) { return "#f768a1";}
+      else if (countryData.LaborForce > 60 && countryData.LaborForce <=80) { return "#c51b8a";}
+      else if (countryData.LaborForce > 80 && countryData.LaborForce <=100) { return "#7a0177";}
 
       return "#DDDDDD";
     })
     .style("filter",function(d){
       var countryData = isoidData.get(d.id);
-      if (countryData) { return "url(#stroke-shadow)";}
-    })
-    ;
+      if (!countryData) { return false;}
+      else if (countryData.LaborForce <=20) {return "url(#stroke-shadow4)";}
+      else if (countryData.LaborForce > 20 && countryData.LaborForce <=40) { return "url(#stroke-shadow3)";}
+      else if (countryData.LaborForce > 40 && countryData.LaborForce <=60) { return "url(#stroke-shadow2)";}
+      else if (countryData.LaborForce > 60 && countryData.LaborForce <=80) { return "url(#stroke-shadow2)";}
+      else if (countryData.LaborForce > 80 && countryData.LaborForce <=100) { return "url(#stroke-shadow)";}
 
-    /**.style("stroke-width",function(d){
-      var countryData = allDataMap.get(d.id);
-      if (!countryData) {return 1;}
-      else if (countryData.LaborForce <= 15) { return 3;}
-      else if (countryData.LaborForce > 75 && countryData.LaborForce <=100) {return 3;}
-      return 1;
-    })
-    .style("stroke-opacity", function(d){
-      var countryData = allDataMap.get(d.id);
-      if (!countryData) {return 0.5;}
-      else if (countryData.LaborForce <= 15) { return 0.8;}
-      else if (countryData.LaborForce > 75 && countryData.LaborForce <=100) {return 0.8;}
-      return 0.5;
-    })**/
+
+        
+    });
   });
- 
+
 });
 
 console.log("after json");
 
 //explanations
 svg.append("rect")
-      .attr("x", width + 23)
+      .attr("x", width + 41)
       .attr("width", 18)
       .attr("height", 18)
-      .style("fill", "#9C4769")
+      .style("fill", "#7a0177")
 svg.append("text")
-    .attr("x",width + 18)
+    .attr("x",width + 28)
     .attr("y",9)
     .attr("dy",".35em")
     .style("text-anchor", "end")
-    .text("100% - 80%");
-
+    .text("80% - 100%");
 
 svg.append("rect")
-      .attr("x", width + 23)
+      .attr("x", width + 41)
       .attr("y",18+2)
       .attr("width", 18)
       .attr("height", 18)
-      .style("fill", "#E7CAD5");
+      .style("fill", "#c51b8a");
 svg.append("text")
-    .attr("x",width + 18)
+    .attr("x",width + 28)
     .attr("y",9+20)
     .attr("dy",".35em")
     .style("text-anchor", "end")
-    .text("80% - 60%");
+    .text("60% - 80%");
 
 svg.append("rect")
-      .attr("x", width + 23)
+      .attr("x", width + 41)
       .attr("y",18+2+20)
       .attr("width", 18)
       .attr("height", 18)
-      .style("fill", "#F0E3E8");
+      .style("fill", "#f768a1");
 svg.append("text")
-    .attr("x",width + 18)
+    .attr("x",width + 28)
     .attr("y",9+40)
     .attr("dy",".35em")
     .style("text-anchor", "end")
-    .text("60% - 40%");
+    .text("40% - 60%");
 
 svg.append("rect")
-      .attr("x", width + 23)
+      .attr("x", width + 41)
       .attr("y",18+2+20+20)
       .attr("width", 18)
       .attr("height", 18)
-      .style("fill", "#C9CFDB");
+      .style("fill", "#fbb4b9");
 svg.append("text")
-    .attr("x",width + 18)
+    .attr("x",width + 28)
     .attr("y",9+60)
     .attr("dy",".35em")
     .style("text-anchor", "end")
-    .text("40% - 20%");
+    .text("20% - 40%");
 
 svg.append("rect")
-      .attr("x", width + 23)
+      .attr("x", width + 41)
       .attr("y",18+2+20+20+20)
       .attr("width", 18)
       .attr("height", 18)
-      .style("fill", "#738AB0");
+      .style("fill", "#feebe2");
 svg.append("text")
-    .attr("x",width + 18)
+    .attr("x",width + 28)
     .attr("y",9+80)
     .attr("dy",".35em")
     .style("text-anchor", "end")
-    .text("20% - 0%");
+    .text("0% - 20%");
 
 svg.append("rect")
-      .attr("x", width + 23)
+      .attr("x", width + 41)
       .attr("y",18+2+20+20+20+20)
       .attr("width", 18)
       .attr("height", 18)
       .style("fill", "#DDDDDD");
 svg.append("text")
-    .attr("x",width + 18)
+    .attr("x",width + 28)
     .attr("y",9+100)
     .attr("dy",".35em")
     .style("text-anchor", "end")
-    .text("No Data");
+    .text("Data Unavailable");
 
 //title
-svg.append("text")
+/*svg.append("text")
     .style("fill","#4F598C")
     .style("font-size","18px")
     .attr("x",320)
     .attr("y",9)
     .attr("dy",".65em")
     .style("text-anchor", "end")
-    .text("Map of Labor Force Participation Rate");
-
-
+    .text("Map of Labor Force Participation Rate");*/
